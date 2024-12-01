299529a84cb167e4f7ca59ec6ef8e8c694550452
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-12-01 15:50:23 +0000

    Worked on some extra functionality

--- Rect Jobs Frontend/src/components/Card.jsx
+++ Rect Jobs Frontend/src/components/Card.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 const Card = ({title, tagline, buttonText, bgColour, btnColour}) => {
+
+  const [buttonTrigger, setButtonTrigger] = useState(false);
+
+  useEffect(() => {
+    if (buttonText == 'Browse Jobs') {
+      setButtonTrigger(true);
+    }
+  }, [])
+
   return (
     <div className={`my-5 mx-5 px-6 py-4 rounded-md w-[40%] bg-[${bgColour}] shadow-lg`}>
             <h1 className='bg-inherit font-bold py-2 text-lg'>{title}</h1>
             <p className='bg-inherit text-md py-1'>{tagline}</p>
-            <button className={`py-1 px-3 my-3 mb-3 bg-[${btnColour}] text-md rounded-md text-white hover:scale-105 transition-all duration-300 shadow-md hover:font-semibold`}>{buttonText}</button>
+
+            {buttonTrigger ? <Link to="/all_jobs"><button className={`py-1 px-3 my-3 mb-3 bg-[${btnColour}] text-md rounded-md text-white hover:scale-105 transition-all duration-300 shadow-md hover:font-semibold`}>{buttonText}</button></Link> : <button className={`py-1 px-3 my-3 mb-3 bg-[${btnColour}] text-md rounded-md text-white hover:scale-105 transition-all duration-300 shadow-md hover:font-semibold`}>{buttonText}</button>}
+
         </div>
   )
 }
