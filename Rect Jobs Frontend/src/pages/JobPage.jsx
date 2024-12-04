3717607759dfe3602af93438bf91fa039482e06a
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-12-04 07:06:21 +0000

    Added in data for job Page, without styling...

--- Rect Jobs Frontend/src/pages/JobPage.jsx
+++ Rect Jobs Frontend/src/pages/JobPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import Spinner from '../components/Spinner';
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
@@ -28,7 +29,40 @@ const JobPage = () => {
 
 
   return loading ? <Spinner /> : (
-  <h1>{job.title}</h1>
+  <div className='max-w-[80%] mx-auto'>
+    <Link to='/all_jobs'><button className=''>Back</button></Link>
+    <div className='grid grid-cols-5'>
+    <div className='col-span-3'>
+    <div>
+    <h1>{job.type}</h1>
+    <h1>{job.title}</h1>
+    <h1>{job.location}</h1>
+    </div>
+    <div>
+        <p>Job Description</p>
+        <p>{job.description}</p>
+
+        <p>Salary</p>
+        <p>{job.salary_range}</p>
+    </div>
+    </div>
+
+    <div className='col-span-2'>
+        <div>
+            <p>Company Info</p>
+            <p>{job.company.name}</p>
+            <p>{job.company.description}</p>
+
+            <p>Contact Email</p>
+            <p>{job.company.contact_email}</p>
+
+            <p>Contact Phone</p>
+            <p>{job.company.contact_phone}</p>
+        </div>
+        <div>Manage Job</div>
+    </div>
+  </div>
+  </div>
 );
 }
 
