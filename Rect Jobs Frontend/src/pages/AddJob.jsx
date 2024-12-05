60cc719f9dd731d771f7cdaa108892f419de4a59
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-12-05 19:56:23 +0000

    finished the styling for the add-job page

--- Rect Jobs Frontend/src/pages/AddJob.jsx
+++ Rect Jobs Frontend/src/pages/AddJob.jsx
@@ -4,36 +4,58 @@ const AddJob = () => {
   return (
     <div className='bg-white'>
       <div className='bg-white py-3'></div>
-      <div className='w-[50%] mx-auto my-[15px] bg-white rounded-lg shadow-lg'>
+      <div className='w-[50%] mx-auto mt-[15px] mb-16 bg-white rounded-lg shadow-lg pb-5'>
         <form>
         <h1 className='bg-inherit text-center text-2xl font-bold py-5 text-[#2ECC71]'>Add Job</h1>
         <div className='px-7'>
         <h2 className='bg-inherit text-lg font-bold py-3 text-black'>Job Info</h2>
-        <div>
+        <div className='mb-4'>
           <label htmlFor="jobType" className='block font-semibold py-1'>Job Type</label>
-          <select name="type" className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'>
+          <select 
+          id='type' 
+          name="type" 
+          required 
+          className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'>
             <option value="Full-Time">Full Time</option>
             <option value="Internship">Internship</option>
             <option value="Part-Time">Part-Time</option>
             <option value="Freelance">Freelance</option>
           </select>
         </div>
-        <div>
-          <label htmlFor="joBTitle" className='block font-semibold py-1'>Title</label>
-          <input type="text" 
+        <div className='mb-4'>
+          <label htmlFor="joBTitle" className='block font-semibold py-1'>
+            Title
+          </label>
+          <input 
+          type="text"
+          id='title'
+          name='title'
+          required
           className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'
           placeholder='eg. Software Engineer'/>
         </div>
-          <div><label htmlFor="jobDescription">Description</label>
+          <div className='mb-4'>
+            <label 
+            htmlFor="jobDescription" 
+            className='block font-semibold py-1'>
+              Description
+            </label>
           <textarea 
           name="jobDescription" 
-          id="jobDexcription"
+          id="jobDescription"
           placeholder='Add any job duties, expectations, requirements, etc'
+          required
+          className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'
+          rows={4}
           ></textarea>
         </div>
-        <div>
-          <label htmlFor="jobSalary">Salary</label>
-          <select name="jobSalary" id="jobSalary">
+        <div className='mb-4'>
+          <label htmlFor="jobSalary" className='block font-semibold py-1'>Salary</label>
+          <select 
+          name="jobSalary" 
+          id="jobSalary" 
+          required 
+          className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'>
             <option value='Under $50K'>Under $50K</option>
             <option value='$50K - 60K'>$50K - $60K</option>
             <option value='$60K - 70K'>$60K - $70K</option>
@@ -47,38 +69,68 @@ const AddJob = () => {
             <option value='Over $200K'>Over $200K</option>
           </select>
         </div>
-        <div>
-          <label htmlFor="jobLocation">Location</label>
-          <input type="text" 
+        <div className='mb-4'>
+          <label htmlFor="jobLocation" className='block font-semibold py-1'>Location</label>
+          <input 
+          className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'
+          id='jobLocation'
+          name='jobLocation'
+          required
+          type="text" 
           placeholder='eg. Miami, FL'/>
         </div>
         </div>
 
         <div className='px-7'>
         <h2 className='bg-inherit text-lg font-bold py-3 text-black'>Company Info</h2>
-        <div>
-          <label htmlFor="companyName">Company Name</label>
-          <input type="text" />
+        <div className='mb-4'>
+          <label htmlFor="companyName" className='block font-semibold py-1'>Name</label>
+          <input 
+          id='companyName'
+          name='companyName'
+          type="text"
+          className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'
+          placeholder='Enter Company Name' />
         </div>
         <div>
-          <label htmlFor="companyDescription">Company Description</label>
-          <textarea name="companyDescription" id="companyDescription"></textarea>
+          <label htmlFor="companyDescription" className='block font-semibold py-1'>Description</label>
+          <textarea 
+          name="companyDescription" 
+          id="companyDescription"
+          rows='4'
+          placeholder='What does your company do?'
+          className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'></textarea>
         </div>
-        <div>
-          <label htmlFor="companyEmail">Company Email</label>
+        <div className='mb-4'>
+          <label 
+          htmlFor="contactEmail"
+          className='block font-semibold py-1'>Contact Email</label>
           <input type="email"
-          id='companyEmail'
+          id='contact_email'
+          name='contact_email'
           placeholder='Email for Applicants'
+          className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'
           />
         </div>
-        <div>
-          <label htmlFor="companyPhone">Company Phone</label>
+        <div className='mb-4'>
+          <label 
+          htmlFor="contactPhone" 
+          className='block font-semibold py-1'>Contact Phone</label>
           <input type="tel" 
-          id='companyPhone'
-            placeholder='Optional Email for Applicants'
+          id='contact_phone'
+          name='contact_phone'
+          placeholder='Optional Email for Applicants'
+          className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'
           />
         </div>
+        
+        <button className='mx-auto w-full text-center bg-[#2ECC71] text-white text-lg my-2 py-2 rounded-xl hover:cursor-pointer hover:bg-emerald-600 transition-all duration-300'
+          type='submit'>
+          Add Job
+        </button>
+
         </div>
+
         </form>
       </div>
     </div>
@@ -94,4 +146,3 @@ export default AddJob
 
 
 
-//bg-gradient-to-br from-[#f9f9fb] to-white
