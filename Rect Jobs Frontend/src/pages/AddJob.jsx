f5ed913196f25a934ff5d785bc6959ef73f9ead7
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-12-06 06:11:21 +0000

    Event Handlers for submitFomr

--- Rect Jobs Frontend/src/pages/AddJob.jsx
+++ Rect Jobs Frontend/src/pages/AddJob.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import { useState } from 'react'
 
 const AddJob = () => {
+  const [type, setType] = useState('Full-Time');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [salary, setSalary] = useState('Under $50K');
+  const [location, setLocation] = useState('');
+  const [companyName, setCompanyName] = useState('');
+  const [companyDescription, setCompanyDescription] = useState('');
+  const [contactEmail, setContactEmail] = useState('');
+  const [contactPhone, setContactPhone] = useState('');
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const newJob = {
+      type,
+      title,
+      description,
+      salary,
+      location,
+      company: {
+        name: companyName,
+        description: companyDescription,
+        contactEmail,
+        contactPhone
+      }
+    }
+  }
+
+
   return (
     <div className='bg-white'>
       <div className='bg-white py-3'></div>
       <div className='w-[50%] mx-auto mt-[15px] mb-16 bg-white rounded-lg shadow-lg pb-5'>
-        <form>
+        <form
+        onSubmit={submitForm}>
         <h1 className='bg-inherit text-center text-2xl font-bold py-5 text-[#2ECC71]'>Add Job</h1>
         <div className='px-7'>
         <h2 className='bg-inherit text-lg font-bold py-3 text-black'>Job Info</h2>
@@ -13,7 +44,9 @@ const AddJob = () => {
           <label htmlFor="jobType" className='block font-semibold py-1'>Job Type</label>
           <select 
           id='type' 
-          name="type" 
+          name="type"
+          value={type} 
+          onChange={(e) => setType(e.target.value)}
           required 
           className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'>
             <option value="Full-Time">Full Time</option>
@@ -31,6 +64,8 @@ const AddJob = () => {
           id='title'
           name='title'
           required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)} 
           className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'
           placeholder='eg. Software Engineer'/>
         </div>
@@ -45,6 +80,8 @@ const AddJob = () => {
           id="jobDescription"
           placeholder='Add any job duties, expectations, requirements, etc'
           required
+          value={description} 
+          onChange={(e) => setDescription(e.target.value)}
           className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'
           rows={4}
           ></textarea>
@@ -55,6 +92,8 @@ const AddJob = () => {
           name="jobSalary" 
           id="jobSalary" 
           required 
+          value={salary} 
+          onChange={(e) => setSalary(e.target.value)}
           className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'>
             <option value='Under $50K'>Under $50K</option>
             <option value='$50K - 60K'>$50K - $60K</option>
@@ -77,6 +116,8 @@ const AddJob = () => {
           name='jobLocation'
           required
           type="text" 
+          value={location} 
+          onChange={(e) => setLocation(e.target.value)}
           placeholder='eg. Miami, FL'/>
         </div>
         </div>
@@ -89,6 +130,8 @@ const AddJob = () => {
           id='companyName'
           name='companyName'
           type="text"
+          value={companyName} 
+          onChange={(e) => setCompanyName(e.target.value)}
           className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'
           placeholder='Enter Company Name' />
         </div>
@@ -99,6 +142,8 @@ const AddJob = () => {
           id="companyDescription"
           rows='4'
           placeholder='What does your company do?'
+          value={companyDescription} 
+          onChange={(e) => setCompanyDescription(e.target.value)}
           className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'></textarea>
         </div>
         <div className='mb-4'>
@@ -109,6 +154,9 @@ const AddJob = () => {
           id='contact_email'
           name='contact_email'
           placeholder='Email for Applicants'
+          required
+          value={contactEmail} 
+          onChange={(e) => setContactEmail(e.target.value)}
           className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'
           />
         </div>
@@ -120,6 +168,8 @@ const AddJob = () => {
           id='contact_phone'
           name='contact_phone'
           placeholder='Optional Email for Applicants'
+          value={contactPhone} 
+          onChange={(e) => setContactPhone(e.target.value)}
           className='rounded-lg w-[100%] bg-[#f4f4f9] p-2 my-1'
           />
         </div>
