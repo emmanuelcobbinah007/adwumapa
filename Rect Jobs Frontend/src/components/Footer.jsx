490f6e7b6fd845e712b80b0fbcf4da33b281bb8e
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-12-08 12:47:07 +0000

    Now done with responsive dsign

--- Rect Jobs Frontend/src/components/Footer.jsx
+++ Rect Jobs Frontend/src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
             <FaYoutube size={30} />
         </div>
         </div>
-        <div className='text-sm lg:col-span-2 flex justify-between mt-6'>
+        <div className='lg:col-span-2 flex justify-between mt-6'>
         <div>
             <h6 className='font-medium text-gray-400'>Solutions</h6>
             <ul>
