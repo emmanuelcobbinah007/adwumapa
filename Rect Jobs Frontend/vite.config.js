be665e669d3795314e6cbea20fb5c88ff20d5b03
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-12-08 10:03:03 +0000

    vercel deployment

--- Rect Jobs Frontend/vite.config.js
+++ Rect Jobs Frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Vercel expects 'dist' as the output directory
+  },
   plugins: [react()],
   server: {
     proxy: {
