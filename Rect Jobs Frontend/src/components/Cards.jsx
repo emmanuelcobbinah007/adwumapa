83bd1f5f70303a5a09f2521dc425920b22edff86
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-12-08 12:04:49 +0000

    Updated the card component

--- Rect Jobs Frontend/src/components/Cards.jsx
+++ Rect Jobs Frontend/src/components/Cards.jsx
@@ -5,7 +5,7 @@ const Cards = () => {
   return (
     <div className='mt-[80px] md:flex md:justify-center max-w-[1200px] mx-auto md:py-12 py-6 bg-[#f9f9fb] '>
         <Card title='For Artisans' tagline='Showcase Your Skills, Unlock New Opportunities' buttonText='Browse Jobs' bgColour='#a4f0c2'/>
-        <Card title='For Employers' tagline='Find the Perfect Match for Your Business' buttonText='Post Jobs' bgColour='#f4f4f9'/>
+        <Card title='For Employers' tagline='Find the Perfect Match for Your Business' buttonText='Post Job' bgColour='#f4f4f9'/>
     </div>
   )
 }
